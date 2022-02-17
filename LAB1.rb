437083c622329d0a3fcb1696cc8c079a87a24746
DmitryBolotsky <bolotsid@gmail.com>
2022-02-17 20:02:15 +0300

    task 1.3

--- LAB1.rb
+++ LAB1.rb
@@ -1,2 +1,16 @@
 #print "Hello #{ARGV}"
-puts String.instance_methods
+# puts String.instance_methods
+
+
+puts "Какой язык программирования вам нравится больше всего"
+lanh = STDIN.gets.chomp
+case lanh
+when "ruby"
+    puts "норм лижешь"
+when "c++"
+  puts "жалко тебя добряк, но будет руби"
+when "c#"
+  puts "ООПшный респект, но будет руби"
+when "kotlin"
+  puts "красиво делаешь, но передумай"
+end
